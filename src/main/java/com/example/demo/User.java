027039a6e4ff36899daa4549e057855b5f9bf820
Roman <romamuhino1@gmai.com>
2024-12-06 14:12:52 +0300

    create connection with db

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("users")
+public class User {
+    @Id
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
